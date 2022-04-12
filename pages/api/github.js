c44b87f35781d7cfdd25bdd7a951aa059a54de6b
frostzzone <65735427+frostzzone@users.noreply.github.com>
2022-04-12 19:40:32 +0000

    [🛠]Fix github bug

--- pages/api/github.js
+++ pages/api/github.js
@@ -5,5 +5,5 @@ export default async (req, res) => {
     const response = await fetch('https://api.github.com/users/' + user);
     const data = await response.json();
     res.setHeader("Content-Type", 'application/json');
-    res.send(JSON.stringify(git(func.data, user), null, 4));
+    res.send(JSON.stringify(func.git(data), null, 4));
 }
